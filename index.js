ee77f5900388bb7483104ab74786b0a6b81c318f
anjana732 <anjanasingh1257@gmail.com>
2024-05-03 14:55:16 +0530

    Configure mongodb database

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
-require ('dotenv').config()
+//require ('dotenv').config()
 const express = require('express')
-
+const { default: mongoose } = require('mongoose')
+//const { DB_NAME } = require('./src/constants')
+//const { default: connectDB } = require('./src/db')
+import { DB_NAME } from './src/constants.js'
+import dotenv from 'dotenv'
+import connectDB from './src/db/index.js'
 const app = express()
-const port = 4000
 
-app.get('/',(req,res) => {
-    res.send("Hello World")
+dotenv.config({
+    path: './env'
 })
 
-app.get('/about', (req,res) =>{
-    res.send("This is about page")
-})
+connectDB()
+//const port = 4000
 
-app.listen(process.env.PORT, () =>{
-    console.log(`Server started at ${port}`)
-})
+
+/*
+
+;( async() => { 
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("ERROR", error);
+            throw error
+        })
+        app.listen(process.env.PORT, () =>{
+            console.log(`Server started at ${port}`)
+        })
+    }catch(error){
+        console.error("ERROR: ", error)
+        throw err
+    }
+})()
+
+*/
