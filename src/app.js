fda3fae0e3cb7d765fe2e37784b058fe5a3f31c9
anjana732 <anjanasingh1257@gmail.com>
2024-07-07 10:28:53 +0530

    Add user controller

--- src/app.js
+++ src/app.js
@@ -4,5 +4,13 @@ import cors from "cors"
 
 const app = express();
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+app.use(cookieParser())
 
+import userRouter from "./routes/user.route.js"
+
+app.use("/api/v1/users",userRouter)
 export {app}
